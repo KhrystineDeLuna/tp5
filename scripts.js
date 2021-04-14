eedbe39d948d8dfd08e6a3c24962de2c5848fdd0
Khrystine Raymond <darqueskyedeluna@gmail.com>
2021-04-14 04:26:42 +0000

    disappearing lists added, adjusted footer; ready to ship

--- scripts.js
+++ scripts.js
@@ -9,9 +9,26 @@ recipeName.onclick = function () {
 };
 
 
-// new HTML in dom //
+// DOM element addition //
 x = document.getElementById("enjoy");
-x.innerHTML = "Enjoy Righteously!";
+x.innerHTML = "Stay tuned to this space for other righteous treats!";
 x.style.fontSize = '1.75em';
 x.style.fontFamily = 'Seymour One';
 x.style.textAlign = 'center';
+
+
+ /* dissapearing lists */
+c1 = document.querySelector("#column1");
+c1.onclick = function () {
+  c1.classList.toggle("clicked");
+};
+
+c2 = document.querySelector("#column2");
+c2.onclick = function () {
+  c2.classList.toggle("clicked");
+};
+
+c3 = document.querySelector("#column3");
+c3.onclick = function () {
+  c3.classList.toggle("clicked");
+};
